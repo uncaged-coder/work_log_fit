ecf9f7560438df5b5b4acb1a6c32bfe69534fb9c
Uncaged Coder <uncaged-coder@proton.me>
2024-06-02 15:52:46 +0000

    Add debuging tools
    one for database dump.
    an another to import some csv files.
    
    Signed-off-by: Uncaged Coder <uncaged-coder@proton.me>

--- lib/hive_manager.dart
+++ lib/hive_manager.dart
@@ -1,7 +1,17 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:work_log_fit/models/program.dart';
+import 'package:work_log_fit/models/exercise.dart';
+import 'package:work_log_fit/models/work_log_entry.dart';
 
 class HiveManager {
   static final HiveManager _instance = HiveManager._privateConstructor();
+  Box? _exercisesBox;
+  Box? _programsBox;
+  Box? _worklogBox;
 
   HiveManager._privateConstructor();
 
@@ -9,15 +19,40 @@ class HiveManager {
     return _instance;
   }
 
-  Box? _exercisesBox;
-  Box? _programsBox;
-  Box? _worklogBox;
-
   // Function to open all necessary boxes
-  Future<void> openAllBoxes() async {
+  Future<void> _openAllBoxes() async {
     _exercisesBox = await Hive.openBox('exercises');
     _programsBox = await Hive.openBox('programs');
     _worklogBox = await Hive.openBox('worklog');
+  }
+
+  void _printHivePath() {
+    if (Hive.isBoxOpen('programs')) {
+      print('Hive programs box path: ${Hive.box('programs').path}');
+    }
+  }
+
+  Future<void> initialize() async {
+    // Set the path for Hive based on the platform
+    if (kIsWeb) {
+      Hive.init('');
+    } else if (Platform.isLinux) {
+      // For Linux, set the path according to XDG Base Directory Specification
+      String homeDirectory = Platform.environment['HOME'] ?? '/home/username';
+      String hiveDirectory = '$homeDirectory/.local/share/work_log_fit/hive';
+      Hive.init(hiveDirectory);
+    } else {
+      // For other platforms, use the path provided by path_provider
+      final appDocumentDir = await getApplicationDocumentsDirectory();
+      Hive.init(appDocumentDir.path);
+    }
+
+    Hive.registerAdapter(ProgramAdapter());
+    Hive.registerAdapter(ExerciseAdapter());
+    Hive.registerAdapter(WorkLogEntryAdapter());
+
+    await _openAllBoxes();
+    _printHivePath();
   }
 
   // Getter for exercises box
--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,18 @@
-import 'dart:io' show Platform, Directory;
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:work_log_fit/models/program.dart';
-import 'package:work_log_fit/models/exercise.dart';
-import 'package:work_log_fit/models/work_log_entry.dart';
 import 'screens/programs_list_screen.dart';
 import 'hive_manager.dart';
 import 'timer.dart';
 import 'settings.dart';
-
-Future<void> _initializeHive() async {
-  await HiveManager().openAllBoxes();
-}
 
 void main() async {
   final GlobalTimerManager globalTimerManager = GlobalTimerManager();
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Set the path for Hive based on the platform
-  if (kIsWeb) {
-    Hive.init('');
-  } else if (Platform.isLinux) {
-    // For Linux, set the path according to XDG Base Directory Specification
-    String homeDirectory = Platform.environment['HOME'] ?? '/home/username';
-    String hiveDirectory = '$homeDirectory/.local/share/work_log_fit/hive';
-    Hive.init(hiveDirectory);
-  } else {
-    // For other platforms, use the path provided by path_provider
-    final appDocumentDir = await getApplicationDocumentsDirectory();
-    Hive.init(appDocumentDir.path);
-  }
-
-  Hive.registerAdapter(ProgramAdapter());
-  Hive.registerAdapter(ExerciseAdapter());
-  Hive.registerAdapter(WorkLogEntryAdapter());
-  printHivePath();
-  await _initializeHive();
+  var hiveManager = HiveManager();
+  await hiveManager.initialize();
 
   runApp(
     Provider<GlobalTimerManager>.value(
@@ -47,12 +20,6 @@ void main() async {
       child: WorkLogFitApp(),
     ),
   );
-}
-
-void printHivePath() {
-  if (Hive.isBoxOpen('programs')) {
-    print('Hive programs box path: ${Hive.box('programs').path}');
-  }
 }
 
 class WorkLogFitApp extends StatelessWidget {
